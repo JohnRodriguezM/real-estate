d1be10a9c53002741094738e8e38d47f83600af7
johnj@John0609 <johnjairorodriguez384@gmail.com>
2023-04-19 22:46:20 -0500

    initial commit

---
+++ src/components/Hero/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+/*export const HeroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  background: #000;
+  color: #fff;
+`;*/
